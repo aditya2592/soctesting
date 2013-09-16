e8721c8129fd966471d0e500a90a7f6dd5afc8ac
Aditya Agarwal <aditya.kgp25@gmail.com>
2013-09-16 18:16:42 +0530

    Added header files

---
+++ soctest.h
@@ -0,0 +1,82 @@
+#ifndef soctest_H
+#define soctest_H
+ 
+
+class tam
+{
+	int tamid;
+	int width;
+	int test_time;
+	public :
+	void store_tam(int id, int w)
+	{
+		tamid = id;
+		width = w;
+		test_time = 0;
+	}
+	int get_width()
+	{
+		return width;
+	}
+	int get_time()
+	{
+		return test_time;
+	}
+	void update_time(int time)
+	{
+		test_time = test_time + time;
+	}
+	int get_tam_id()
+	{
+		return tamid;
+	}
+};
+class core
+{
+	int coreid;
+	int is_assg;
+	tam mytam;
+	public:
+	void store_id(int id)
+	{
+		coreid = id;
+		is_assg = 0;
+	}
+	void update_assg(tam t)
+	{
+		is_assg = 1;
+		mytam = t;
+
+	}
+	int is_assgn()
+	{
+		if(is_assg)
+			return 1;
+		else
+			return 0;
+	}
+	tam get_core_tam()
+	{
+		return mytam;
+	}
+};
+class times
+{
+	core cr;
+	tam ta;
+	int test_time;
+	public:
+	void store_test(core c, tam t, int time)
+	{
+		cr = c;
+		ta = t;
+		test_time = time;
+	}
+	int get_time()
+	{
+		return test_time;
+	}
+};
+
+//Below here added on Tuesday//
+#endif
